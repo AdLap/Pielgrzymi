e75155bd8593d111307d9da6952ccf02e534627d
Adam <adamlapacz@gmail.com>
2021-04-22 18:37:17 +0200

    fixed menu media

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@
 
 const menuAnimation = document.querySelector('#burger');
 const menu = document.querySelector('#menu');
+const media = window.matchMedia('(min-width: 750px)');
+
+
+// BURGER ANIMATION
 
 menuAnimation.addEventListener('click', function () {
     menuAnimation.classList.toggle('menu-visible');
@@ -19,3 +23,17 @@ menu.addEventListener('click', function () {
     menu.style.transform = 'translate(0)';
     menuAnimation.classList.remove('menu-visible');
 });
+
+media.addEventListener('change', function (e) {
+    if (e.matches) {
+        menuAnimation.classList.remove('menu-visible');
+        menu.style.transform = 'translateY(0)';
+    }
+
+    if (!e.matches) {
+        menu.style.transform = 'translateY(-215px)';
+    }
+});
+
+// FORM VALIDATION
+

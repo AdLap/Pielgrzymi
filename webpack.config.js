83e17ec27bc0f427aaed0fb3b63b4fcafe8759e7
Adam <adamlapacz@gmail.com>
2021-06-22 01:26:17 +0200

    fix configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
 const path = require('path');
-const autoprefixer = require('autoprefixer');
-const Html = require('html-webpack-plugin');
 
 module.exports = {
     entry: "./js/app.js",
@@ -13,11 +11,9 @@ module.exports = {
         contentBase: path.join(__dirname, "public"),
         publicPath: "/build",
         compress: true,
-        port: 3001
+        port: 3001,
+        historyApiFallback: true
     },
-    mode: "development",
-    devtool: 'source-map',
-    watch: true,
     module: {
         rules: [
             {
@@ -25,64 +21,6 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: "babel-loader"
             },
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                use: ['style-loader',
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: () => [autoprefixer()]
-                        }
-                    },
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            },
-            {
-                test: /\.(jpe?g|gif|png|svg)$/,
-                loader: 'file-loader',
-                options: {
-                    name: '[name].[ext]',
-                    publicPath: '/images/',
-                    outputPath: '/images/'
-                }
-            },
-            {
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    }
-                ]
-            }
         ]
     },
-    plugins: [
-        new Html({
-            filename: 'index.html',
-            template: './index.html'
-        })
-    ]
 }

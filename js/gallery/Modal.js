1a561e0fd64697852debea9cbf62d0560c17ec16
Adam <adamlapacz@gmail.com>
2021-08-05 17:31:25 +0200

    gallery working

--- js/gallery/Modal.js
+++ js/gallery/Modal.js
@@ -1,31 +1,15 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowCircleLeft, faArrowCircleRight, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 export const Modal = ({ pic }) => {
-    const styleModal = {
-        width: '90vw',
-        height: '90vh',
-        position: 'fixed',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        overflow: 'hidden'
-    };
-
-    const stylePic = {
-        width: '100%',
-        height: 'auto',
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)'
-    }
-
     return (
         <>
-            <div className='movie__galllery__modal' style={styleModal}>
-                <div className='movie__gallery__modal__prev'></div>
-                <div className='movie__gallery__modal__next'></div>
-                <img src={pic} alt='PBM picture' className='movie__gallery__modal__img' style={stylePic}/>
+            <div className='movie__galllery__modal'>
+                <div className='movie__gallery__modal__prev'><FontAwesomeIcon icon={faArrowCircleLeft} /></div>
+                <div className='movie__gallery__modal__next'><FontAwesomeIcon icon={faArrowCircleRight} /></div>
+                <div className='movie__gallery__modal__close'><FontAwesomeIcon icon={faTimesCircle} /></div>
+                <img src={pic} alt='PBM picture' className='movie__gallery__modal__img'/>
             </div>
         </>
     );
